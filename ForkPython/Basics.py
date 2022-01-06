7eb2c60ad4de53cf43ae4ca751626b3ee51118a6
Mansi Wakhankar <mansiswakharkar@gmail.com>
2022-01-06 21:18:03 +0300

    Basics of Python

---
+++ ForkPython/Basics.py
@@ -0,0 +1,22 @@
+#First Script
+print("Hello World")
+
+#Variable declarations
+num1 = 4
+print(num1)
+num2 = 4.5
+print(num2)
+str1 = "Movie Time"
+print(str1)
+#List of mixed datatypes
+list1 = []
+list1.append(num1)
+list1.append(num2)
+list1.append(str1)
+print(list1)
+
+#Input - Output
+name = input("Enter your name: ")
+num1 = int(input("Enter number"))
+print(name)
+print(num1)
